0744e8ed3b5ac0b22d13bd0d9f60c49444f36cd8
Smaton1 <sam_maton@hotmail.com>
2015-03-08 17:34:36 +0000

    added the function to generate the board, still need to add flip card function

--- scripts/memory.js
+++ scripts/memory.js
@@ -16,5 +16,18 @@ Array.prototype.memory_tile_shuffle = function(){
     temp = this[j];
     this[j] = this[i];
     this[i] = temp;
-  }
-}
+  };
+};
+
+
+function newBoard(){
+  tiles_flipped = 0;
+  var output = '';
+  memory_array.memory_tile_shuffle();
+  for(var i = 0; i <memory_array.length; i++) {
+    output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+  };
+  document.getElementById('memory_board').innerHTML = output;
+
+  
+};
